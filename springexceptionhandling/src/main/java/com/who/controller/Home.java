3082031935380b8d07fb166e6c886a8ee83c131a
jptjava <jptjava>
2016-03-29 12:06:31 -0500

    rr

--- springexceptionhandling/src/main/java/com/who/controller/Home.java
+++ springexceptionhandling/src/main/java/com/who/controller/Home.java
@@ -1,8 +1,18 @@
 package com.who.controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.who.exception.AddressNotFoundException;
+import com.who.exception.UserNotFoundException;
 
 @Controller
 public class Home {
@@ -11,6 +21,62 @@ public class Home {
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String home(){
 		return "home";
+	}
+	
+	/**
+	 * First way of handling exception-"Exception handler method".
+	 * 
+	 * This method throws UserNotFoundException and will be handled by Exception Handler method in 
+	 * this same controller class
+	 */
+	
+	@RequestMapping(value="/user",method=RequestMethod.GET)
+	public String user(){
+		throw new UserNotFoundException(1);
+	}
+	
+	/**
+	 * Second way of handling exception is "global exception handler class"
+	 * 
+	 * This method throws SQLException and this exception is handled by
+	 * global exception handler that uses @ControllerAdvice annotation
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/data",method=RequestMethod.GET)
+	public String data() throws Exception{
+		throw new SQLException("SQLException, id="+2);
+	}
+	
+	@RequestMapping(value="/file",method=RequestMethod.GET)
+	public String file() throws Exception{
+		throw new IOException("IOException, id="+3);
+	}
+	
+	/**
+	 * Third way is to configured the exception handler in xml using "SimpleMappingExceptionResolver"
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/address",method=RequestMethod.GET)
+	public String address(){
+		System.out.println("sdfsd");
+		throw new AddressNotFoundException(4);
+	}
+	
+	
+	
+	
+	//This is a exception handler method
+	@ExceptionHandler(UserNotFoundException.class)
+	public String handleUserNotFoundException(HttpServletRequest request,Exception ex,Model model){
+		
+		System.out.println("Requested URL "+ request.getRequestURL());
+		System.out.println("Exception Raised "+ ex);
+		model.addAttribute("exception", ex);
+		model.addAttribute("url", request.getRequestURL());
+		return "error";
+		
 	}
 
 }
--- springexceptionhandling/src/main/java/com/who/exception/UserNotFoundException.java
+++ springexceptionhandling/src/main/java/com/who/exception/UserNotFoundException.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="User Not Found") //404
-public class UserNotFoundException extends Exception{
+public class UserNotFoundException extends RuntimeException{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2744568940790986681L;
+	
 	
 	
 	public UserNotFoundException(int id){
