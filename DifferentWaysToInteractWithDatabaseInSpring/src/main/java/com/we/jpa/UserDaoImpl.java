28300cf28a707a5be28398ab71ff9725dfb11034
jptjava <jptjava>
2016-03-31 09:59:36 -0500

    ww

--- DifferentWaysToInteractWithDatabaseInSpring/src/main/java/com/we/jpa/UserDaoImpl.java
+++ DifferentWaysToInteractWithDatabaseInSpring/src/main/java/com/we/jpa/UserDaoImpl.java
@@ -1,13 +1,10 @@
 package com.we.jpa;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -34,16 +31,5 @@ public class UserDaoImpl implements UserDao {
 
 	}
 
-	private class UserMapper implements RowMapper<User> {
-
-		@Override
-		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-			User user = new User();
-			user.setId(rs.getInt("ID"));
-			user.setUsername(rs.getString("USERNAME"));
-			user.setPhoneNumber(rs.getInt("PHONE_NUMBER"));
-			return user;
-		}
-
-	}
+	
 }
